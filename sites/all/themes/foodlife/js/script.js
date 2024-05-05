b88f26b841e317a9f23e8a46a6905758bca4e84a
devac123 <imporgrammer.shubham@gmail.com>
2024-05-05 09:47:37 -0700

    remve comments

--- sites/all/themes/foodlife/js/script.js
+++ sites/all/themes/foodlife/js/script.js
@@ -1,17 +1,3 @@
-// console.log(jQuery('.block-views-product-search-block-block'));
-// var dietTagWrapperParent =  jQuery('.block-views-product-search-block-block').find('.views-widget-filter-field_diet_tags_tid').find('.views-widget')
-// dietTagWrapperParent.prepend('<div class="diet_tag_cards card-stack"></div>')
-// var dietTagWrapper =  jQuery('.block-views-product-search-block-block').find('.edit-field-diet-tags-tid')
-
-// dietTagWrapper.each(function(index){
-// console.log(jQuery(this))
-    // htemlElemwnt = '<div class="product_type_card" id="diet_tag_card-' + item + '" data-id="' + radio_id + '">' + ProductName + '</div>'
-    // if(index==0){
-    //     jQuery('.diet_tag_cards').append()
-    // }
-// })
-
-// console.log(dietTagWrapper);
 
 (function($) {
     Drupal.behaviors.myCustomBehavior = {
